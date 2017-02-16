8579a108b280f3a8b8c7e92d920c0adbe8056fc1
Dave Vieglais <datadavev@users.noreply.github.com>
2017-02-16 01:02:21 +0000

    switch to sphinx RTD theme

--- source/conf.py
+++ source/conf.py
@@ -202,9 +202,12 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 #html_theme = 'default'
-html_theme = 'dataone_sans'
+#html_theme = 'dataone_sans'
 #html_theme = 'dataone'
-#html_theme = 'sphinx_rtd_theme'
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
