4cff0b004ca9c2c8d90713cb1d0ed630762ad887
Dave Vieglais <datadavev@users.noreply.github.com>
2020-11-23 21:48:50 -0500

    Bumping version to help RTD

--- source/conf.py
+++ source/conf.py
@@ -55,7 +55,7 @@ previous_version = "2.1.0"
 # The short X.Y version.
 version = "2.2"
 # The full version, including alpha/beta/rc tags.
-release = "2.2.1"
+release = "2.2.2"
 
 
 # -- General configuration -----------------------------------------------------
